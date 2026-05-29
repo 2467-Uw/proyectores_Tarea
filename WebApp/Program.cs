456f7c565334cbff6b015a681a68c36640039329
2467-Uw <C23470291@campeche.tecnm.mx>
2026-05-29 11:07:56 -0600

    se realizaron cambios hacerca de los proyectores

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,8 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using WebApp.Data.Migrations;
-using WebApp.Models.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
