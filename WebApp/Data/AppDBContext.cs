93951065e95c6e410ed21739773cf9275d265fe0
2467-Uw <C23470291@campeche.tecnm.mx>
2026-06-05 11:39:12 -0600

    Realiza el sembrado de datos

--- WebApp/Data/AppDBContext.cs
+++ WebApp/Data/AppDBContext.cs
@@ -19,7 +19,47 @@ namespace WebApp.Data.Migrations
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<Proyector>().ToTable("Proyectores");
-            modelBuilder.Entity<Departamento>().ToTable("Departamentos");
+            modelBuilder.Entity<Departamento>().ToTable("Departamentos") .HasData(
+                new Departamento
+                { 
+                    Id = 1, Titulo = "Departamento de Sistemas y Computación" 
+                },
+                new Departamento
+                {
+                    Id = 2,
+                    Titulo = "Departamento de Ingeniería Industrial" 
+                },
+                new Departamento
+                {
+                    Id = 3,
+                    Titulo = "Departamento de Ingeniería Civil"
+                },
+                new Departamento
+                {
+                    Id = 4,
+                    Titulo = "Departamento de Ciencias de la Tierra"
+                },
+                new Departamento
+                {
+                    Id = 5,
+                    Titulo = "Departamento de Ciencias Economico-Administrativas"
+                },
+                new Departamento
+                {
+                    Id = 6,
+                    Titulo = "Departamento de Ingeniería Mecánica"
+                },
+                new Departamento
+                {
+                    Id = 7,
+                    Titulo = "Departamento de Ingeniería Química"
+                },
+                new Departamento
+                {
+                    Id = 8,
+                    Titulo = "Departamento de Ciencias Basicas"
+                }
+            );
         }
 
     }
---
+++ WebApp/Migrations/20260605173353_DataSeededDepartamentos.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataSeededDepartamentos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Departamentos",
+                columns: new[] { "Id", "Titulo" },
+                values: new object[,]
+                {
+                    { 1, "Departamento de Sistemas y Computación" },
+                    { 2, "Departamento de Ingeniería Industrial" },
+                    { 3, "Departamento de Ingeniería Civil" },
+                    { 4, "Departamento de Ciencias de la Tierra" },
+                    { 5, "Departamento de Ciencias Economico-Administrativas" },
+                    { 6, "Departamento de Ingeniería Mecánica" },
+                    { 7, "Departamento de Ingeniería Química" },
+                    { 8, "Departamento de Ciencias Basicas" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Departamentos",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Departamentos",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Departamentos",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Departamentos",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Departamentos",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Departamentos",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Departamentos",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Departamentos",
+                keyColumn: "Id",
+                keyValue: 8);
+        }
+    }
+}
